b68fe5edefd4d99a08a3909c8efa0716f264d461
Omar1111111y <106968928+Omar1111111y@users.noreply.github.com>
2022-07-22 12:08:46 +0300

    Add files via upload

---
+++ bot_detd.py
@@ -0,0 +1,12 @@
+import os
+import random
+import requests
+import json
+from bot_imop import * 
+#from tkinter import filedialog
+from duce import * 
+bot = telebot.TeleBot(botmm)
+def rmm(ms):
+	iff = ms.text
+	bot.reply_to(ms,'مطور هذا البوت عمر ستايل')
+#	os.system('python duce.py')
