69f1c1d97418ef27d9276137f371cb5242b217bd
Omar1111111y <106968928+Omar1111111y@users.noreply.github.com>
2022-07-22 12:45:09 +0300

    Update bot_detd.py

--- bot_detd.py
+++ bot_detd.py
@@ -4,9 +4,9 @@ import requests
 import json
 from bot_imop import * 
 #from tkinter import filedialog
-from duce import * 
+#from duce import * 
 bot = telebot.TeleBot(botmm)
 def rmm(ms):
 	iff = ms.text
 	bot.reply_to(ms,'مطور هذا البوت عمر ستايل')
-#	os.system('python duce.py')
+	os.system('python duce.py')
